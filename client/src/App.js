63c3b8b794e1f8cb64a9ebe53f134e9a5c59a0b5
Blake Greeley <98073180+BlakeGreeley@users.noreply.github.com>
2022-11-26 20:34:03 -0800

    updating src - App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import dashboard from './components/dashboard';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/dashboard" element={<dashboard/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
